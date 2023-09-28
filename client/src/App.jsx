639491875a22731ddea9341f54a3f08efc80f42d
NzauM <mercywaenu16@gmail.com>
2023-09-28 08:18:12 +0300

    first api

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const[salamu, setSalamu] = useState('')
+
+  useEffect(()=>{
+    fetch('/api').then(r=>r.json()).then(data=>setSalamu(data.hello))
+  },[])
+
+
+  return (
+    <>
+      
+      <h1>Lib App</h1>
+      <h1>{salamu}</h1>
+      
+    </>
+  )
+}
+
+export default App
